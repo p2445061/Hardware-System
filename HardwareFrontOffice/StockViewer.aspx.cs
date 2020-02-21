1fe0a5c17158d9adce3855a680e674c110dc6ecb
George Kyriacou <P17183016@my365.dmu.ac.uk>
2020-02-21 13:33:14 +0000

    Finished lab19

--- HardwareFrontOffice/StockViewer.aspx.cs
+++ HardwareFrontOffice/StockViewer.aspx.cs
@@ -1,29 +1,25 @@
-﻿
+﻿using HardwareClasses;
 using System;
-using HardwareClasses;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
 
-    public partial class stock : System.Web.UI.Page
+public partial class StockViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            clsStock stock = new clsStock();
-            stock = (clsStock)Session["stock"];
-            Response.Write(stock.PartNo);
-           
+        clsStock stock = new clsStock();
 
+        stock = (clsStock)Session["stock"];
+        Response.Write(stock.PartNo);
     }
-   
 
     protected void btnOK_Click(object sender, EventArgs e)
-        {
-            clsStock stock = new clsStock();
-            stock.PartNo = txtPartNo.Text;
+    {
+        clsStock stock = new clsStock();
+
         stock.PartNo = txtPartNo.Text;
         stock.SupplierId = Convert.ToInt32(this.txtSupplier.Text);
         stock.PartDescription = txtPartDescription.Text;
@@ -32,13 +28,6 @@ using System.Web.UI.WebControls.WebParts;
         stock.Quantity = Convert.ToInt32(this.txtQuantity.Text);
 
 
-        Session["stock"] = stock;
 
-            Response.Redirect("stock.aspx");
-         }
-
-
-
-
-    
+    }
 }
