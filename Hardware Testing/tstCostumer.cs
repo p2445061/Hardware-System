a12fd7818c3ae657154c36089e3d6e3da07c4808
nataliast00 <p2446090@dmu.ac.uk>
2020-02-21 14:09:21 +0000

    I finished the lab sheet 19 but i have some errors at the web forms at the end of the sheet

--- Hardware Testing/tstCostumer.cs
+++ Hardware Testing/tstCostumer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using HardwareClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HardwareClasses;
+
 
 namespace Hardware_Testing
 {
@@ -17,5 +19,117 @@ namespace Hardware_Testing
             
 
         }
+
+        [TestMethod]
+        public void ActivePropertOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            ACostumer.Active = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(ACostumer.Active, TestData);
+        }
+
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            ACostumer.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.DateAdded, TestData);
+        }
+
+
+        [TestMethod]
+        public void AddressNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            ACostumer.AddressNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.AddressNo, TestData);
+
+   
+        }
+
+
+        [TestMethod]
+        public void CountyNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            ACostumer.CountyNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.CountyNo, TestData);
+        }
+
+        [TestMethod]
+        public void HouseNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            string TestData = "21b";
+            //assign the data to the property
+            ACostumer.HouseNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.HouseNo, TestData);
+        }
+
+
+        [TestMethod]
+        public void PostCodePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            string TestData = "LE1 4AB";
+            //assign the data to the property
+            ACostumer.PostCode = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.PostCode, TestData);
+        }
+
+        [TestMethod]
+        public void StreetPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            string TestData = "Some Street";
+            //assign the data to the property
+            ACostumer.Street = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.Street, TestData);
+        }
+
+
+        [TestMethod]
+        public void TownPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCostumer ACostumer = new clsCostumer();
+            //create some test data to assign to the property
+            string TestData = "Leicester";
+            //assign the data to the property
+            ACostumer.Town = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACostumer.Town, TestData);
+        }
+
     }
 }
---
+++ HardwareFrontOffice/ACostumer.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ACostumer.aspx.cs" Inherits="ACostumer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        House No&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtHouseNo" runat="server"></asp:TextBox>
+        <br />
+        Street&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="textStreet" runat="server"></asp:TextBox>
+        <br />
+        Town&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtTown" runat="server"></asp:TextBox>
+        <br />
+        Post Code&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtPostCode" runat="server"></asp:TextBox>
+        <br />
+        Country&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtCountry" runat="server"></asp:TextBox>
+        <br />
+        Date Added&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtDateAdded" runat="server"></asp:TextBox>
+        <br />
+        Date Of Birth&nbsp;
+        <asp:TextBox ID="txtDateOfBirth" runat="server"></asp:TextBox>
+        <br />
+        Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <br />
+        Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+        <br />
+        Active&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtActive" runat="server"></asp:TextBox>
+        <br />
+    </form>
+</body>
+</html>
