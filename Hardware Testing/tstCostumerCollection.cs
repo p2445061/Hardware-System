7adb4984d205b77af6ddc3a19d504df6a47fb5b2
Natalia Stylianou <P2446090@my365.dmu.ac.uk>
2020-03-31 01:58:22 +0100

    Left One lab and need to fix some other things

--- Hardware Testing/tstCostumerCollection.cs
+++ Hardware Testing/tstCostumerCollection.cs
@@ -43,6 +43,73 @@ namespace Test_Framework
         }
 
 
+        [TestMethod]
+        
+        public void  CountPropertyOK()
+        {
+            clsCostumerCollection AllCostumers = new clsCostumerCollection();
+
+            Int32 SomeCount = 2;
+
+            AllCostumers.Count = SomeCount;
+
+            Assert.AreEqual(AllCostumers.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisCostumerPropertyOK()
+        {
+            clsCostumerCollection AllCostumers = new clsCostumerCollection();
+
+            clsCostumer TestCostumer = new clsCostumer();
+
+            TestItem.Active = true;
+            TestItem.CostumerID = 1;
+            TestItem.CostumerDOB = DateTime.Now.Date;
+            TestItem.CostumerAddress = "22 Grange Lane LE2 7NX";
+            TestItem.Name = "Natalia Stylianou";
+            TestItem.Email = "ns@gmail.com";
+
+
+            AllCostumers.ThisCostumer = TestCostumer;
+
+            Assert.AreEqual(AllCostumers.ThisCostumer, TestCostumer);
+        }
+
+
+       // [TestMethod]
+       // public void ListAndCountOK()
+       // {
+        //    clsCostumerCollection AllCostumers = new clsCostumerCollection();
+
+        //    List<clsCostumer> TestList = new List<clsCostumer>();
+
+        //    clsCostumer TestItem = new clsCostumer();
+
+      //    TestItem.Active = true;
+      //     TestItem.CostumerID = 1;
+      //    TestItem.CostumerDOB = DateTime.Now.Date;
+      //     TestItem.CostumerAddress = "22 Grange Lane LE2 7NX";
+      //     TestItem.Name = "Natalia Stylianou";
+      //     TestItem.Email = "ns@gmail.com";
+
+      //     TestList.Add(TestItem);
+
+      //      AllCostumers.CostumerList = TestList;
+
+      //      Assert.AreEqual(AllCostumers.Count, TestList.Count);
+      //  }
+
+
+     //   [TestMethod]
+     //   public void TwoRecordsPresent()
+     //   {
+     //       clsCostumerCollection AllCostumers = new clsCostumerCollection();
+
+     //      Assert.AreEqual(AllCostumers.Count, 2);    
+     //   }
+
+
 
     }
 }
--- HardwareClasses/clsCostumerCollection.cs
+++ HardwareClasses/clsCostumerCollection.cs
@@ -3,15 +3,66 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Class_Library;
 using System.Collections.Generic;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+
+public class clsCostumerCollection
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    List<clsCostumer> mCostumerList = new List<clsCostumer>();
+
+    public List<clsCostumer> CostumerList
+    {
+        get
+        {
+            return mCostumerList;
+        }
+        set
+        {
+            mCostumerList = value;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return mCostumerList.Count;
+        }
+        set
+        {
+            //
+        }
+    }
+
+    public clsCostumerCollection()
+    {
+        Int32 Index = 0;
+
+        Int32 RecordCount = 0;
+
+        clsDataConnection DB = new clsDataConnection();
+
+        DB.Execute("sproc_tblCostumer_SelectAll");
+
+        RecordCount = DB.Count;
+
+        while (Index < RexordCount)
+        {
+            clsCostumerCollection ACostumer = new clsCostumerCollection();
+
+            ACostumer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+            ACostumer.CostumerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CostumerID"]);
+            ACostumer.CostumerDOB = Convert.ToDateTime(DB.DataTable.Rows[Index]["CostumerDOB"]);
+            ACostumer.CostumerAddress = Convert.ToString(DB.DataTable.Rows[Index]["CostumerAddress"]);
+            ACostumer.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+            ACostumer.Address= Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+
+            mCostumerList.Add(ACostumer);
+
+            Index++;
+        }
+    }
+
+
+
+
 }
+
